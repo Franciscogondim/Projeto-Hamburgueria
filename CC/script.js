80be0e2a4f24ff3a5c2027276570bdd4e99ffb63
matheus-barbosa27 <matheus09bg@gmail.com>
2026-05-14 20:33:34 -0300

    Update script.js

--- CC/script.js
+++ CC/script.js
@@ -1,1 +1,22 @@
+// para fazer o pop-up funcionar.
+const abrirPopUp = document.querySelectorAll('.abrirPopUp')
 
+abrirPopUp.forEach(button => {
+    button.addEventListener('click', () => {
+        const popUpId = button.getAttribute('data-popUp');
+        const popUp = document.getElementById(popUpId)
+       
+        popUp.showModal();
+    });
+});
+
+const botaoX = document.querySelectorAll('.botaoX');
+
+botaoX.forEach(button => {
+    button.addEventListener('click', () => {
+        const popUpId = button.getAttribute('data-popUp');
+        const popUp = document.getElementById(popUpId);
+        
+        popUp.close();
+    });
+});
